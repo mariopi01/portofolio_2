8c0abc524ad43bf4db2974af367dfc4af5ed9378
mariopi <mariopi.wac@gmail.com>
2025-11-12 20:49:31 +0700

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,66 @@
 //   plugins: [
 //     react({
 //       babel: {
-//         plugins: [['babel-plugin-react-compiler'] ],
+//         plugins: [['babel-plugin-react-compiler'],tailwindcss(), ], 
 //       },
 //     }),
 //   ],
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     // 1. Plugin React
+//     react(),
+
+//     // 2. Plugin Tailwind
+//     // Tempatkan di sini (sebagai plugin Vite), BUKAN di dalam 'babel'.
+//     tailwindcss(),
+//   ],
+// })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     // 1. Plugin React
+//     react({
+//       // AKTIFKAN BARIS INI:
+//       // Ini memberi tahu Vite untuk menggunakan React Compiler
+//       // (yang sudah Anda install)
+//       compiler: true,
+//     }),
+
+//     // 2. Plugin Tailwind
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vite.config/
 export default defineConfig({
   plugins: [
     // 1. Plugin React
-    react(),
+    react({
+      // Tambahkan baris ini untuk mengaktifkan React Compiler
+      compiler: true, 
+    }),
 
     // 2. Plugin Tailwind
-    // Tempatkan di sini (sebagai plugin Vite), BUKAN di dalam 'babel'.
     tailwindcss(),
   ],
 })
+
